b828f8d7d885fb8619adffb1b413d55fb29abae2
shubham638777 <shubhamsingh26067@gmail.com>
2025-09-16 15:37:46 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,35 @@
+// // let a  = 13;
+// // console.log(a);
+// // {
+// //      let a = 43;
+// //     console.log(a);
+// // }
+
+// // console.log(a);
+
+
+
+// // function output(){
+// //     var x = y = 8;
+// // }
+// // output();
+// // console.log(typeof x);
+// // console.log(typeof y);
+
+// console.log(typeof null);
+// console.log(typeof NULL);
+// console.log(typeof NULL);
+// console.log(typeof NULL);
+// console.log(typeof NaN)
+// console.log(typeof undefined);
+
+// let a = "235" - 23 + 23 + true - false + 23 - 235 + 235   //the value of true is 1 and the value of false is 0   // "235" ka mtlb cocatinate (add)  krana hoga
+// console.log(a)
+
+
+// let b= "235" - 23 + 23 + "true" - false + 23 - 235 + 235  // string k form me alphabate(true) h isiliye output NaN aayega
+// console.log(b)    // agr "true k place pr 1 hota to as it is solve kr dete"
+
+
+let a = (true + false)>2 + 3 +  true;
+console.log(a)
